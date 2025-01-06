2160ecf50c723c59e1825c0f5177411cc419bd53
trueSlav <2001idercach@gmail.com>
2025-01-07 00:27:02 +0300

    feat(database): add prisma
    prisma is configured. A database model has been created. migration from the database has been performed

---
+++ backend/prisma/migrations/20250106204703_init/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `surname` VARCHAR(191) NOT NULL,
+    `patronymic` VARCHAR(191) NOT NULL,
+    `department` VARCHAR(191) NOT NULL,
+    `hire_date` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Department` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
